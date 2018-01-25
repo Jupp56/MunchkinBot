693b03f6ffb28548ae4863417765fa3caa4e6331
Jupp56 <Nick@NICKPC>
2018-01-25 19:22:10 +0100

    Neue Klasse für die Kartenstackverwaltung. Erste Methoden (ohne Funktion)

---
+++ MunchkinBot/Kartenstack.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MunchkinBot
+{
+    class Kartenstack
+    {
+        public int tuerkarte()
+        {
+            int ID = -1;
+            return ID;
+        }
+
+        public int schatzkarte()
+        {
+            int ID = -1;
+            return ID;
+        }
+    }
+}
