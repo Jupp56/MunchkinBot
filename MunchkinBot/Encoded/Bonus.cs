a926d73c760cbb441e73b32c188a9536a627886e
Olfi01 <flom@meyer-alpers.de>
2018-02-01 10:44:40 +0100

    Basics of the parser

--- MunchkinBot/Encoded/Bonus.cs
+++ MunchkinBot/Encoded/Bonus.cs
@@ -4,6 +4,16 @@
     {
         private short[] boni = new short[200];
 
+        public Bonus(string from)
+        {
+            string[] blocks = from.Split(';');
+            foreach(string block in blocks)
+            {
+                string[] split = block.Split(':');
+                boni[int.Parse(split[0])] = short.Parse(split[1]);
+            }
+        }
+
         public short this[int index]
         {
             get
