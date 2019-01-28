7182ef25a86af86b957eb84faa31ecf9f745d506
Florian Meyer <florian.meyer@capgemini.com>
2019-01-28 12:26:20 +0100

    config update

--- MunchkinBot/Program.cs
+++ MunchkinBot/Program.cs
@@ -79,8 +79,6 @@ namespace MunchkinBot
 
             stopEvent.WaitOne();
             StopBot();
-
-
         }
         #endregion
 
--- MunchkinBotControl/first.cmd
+++ MunchkinBotControl/first.cmd
@@ -1,3 +1,4 @@
 @echo OFF
 git clone https://github.com/Jupp56/MunchkinBot
+cd MunchkinBot
 git checkout flom-rework
