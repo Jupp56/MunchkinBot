439e206c3e9de388831a92521369685329968440
Florian Meyer <florian.meyer@capgemini.com>
2019-09-11 13:31:27 +0200

    StickerId command

--- MunchkinBot/Commands/StandaloneCommands.cs
+++ MunchkinBot/Commands/StandaloneCommands.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Telegram.Bot.Types.Enums;
 
 namespace MunchkinBot.Commands
 {
@@ -14,6 +15,13 @@ namespace MunchkinBot.Commands
         {
             await Task.CompletedTask;
             throw new NotImplementedException();
+        }
+
+        [Command("/stickerid", Description = "Sends back the sticker ID", Usage = "/stickerid", RequiredContext = CommandAttribute.Context.Private, RequiresGlobalAdmin = true, Standalone = true)]
+        public static async Task StickerId(CommandContext context)
+        {
+            if (context.Message.Type != MessageType.Sticker) return;
+            await context.Bot.SendTextMessageAsync(context.Message.Chat.Id, context.Message.Sticker.FileId);
         }
     }
 }
